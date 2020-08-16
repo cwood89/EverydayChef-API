3199d42adbc0352c1b0fe84c841550c67a7c2893
cwood89 <cwood89@mac.com>
2020-08-16 14:23:14 -0400

    working on save route

--- src/main/java/com/everydaychef/main/controller/RecipeController.java
+++ src/main/java/com/everydaychef/main/controller/RecipeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -35,7 +36,7 @@ public class RecipeController {
   }
 
   @PostMapping("/recipes")
-  public Recipe saveRecipe(Recipe recipe) {
+  public Recipe saveRecipe(@RequestBody Recipe recipe) {
 
     return apiService.saveRecipe(recipe);
   }
