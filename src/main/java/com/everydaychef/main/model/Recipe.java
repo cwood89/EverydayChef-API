a118a6a9c2de1f268cbffa3bdba2917e05c0f766
cwood89 <cwood89@mac.com>
2020-08-17 09:23:00 -0400

    attempt to parse uri

--- src/main/java/com/everydaychef/main/model/Recipe.java
+++ src/main/java/com/everydaychef/main/model/Recipe.java
@@ -21,9 +21,9 @@ public class Recipe {
   private Double totalTime;
   private String[] ingredientLines;
 
-  public Recipe(String label, String image, String source, String url, Double yield, Double totalTime,
+  public Recipe(String uri, String label, String image, String source, String url, Double yield, Double totalTime,
       String[] ingredientLines) {
-    this.recipeId = this.parseId(this.uri);
+    this.recipeId = this.parseId(uri);
     this.label = label;
     this.image = image;
     this.url = url;
