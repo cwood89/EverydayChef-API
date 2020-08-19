ef13838e08486b5f6d5af6dcbbd30ad1dfdeaa87
cwood89 <cwood89@mac.com>
2020-08-19 09:14:44 -0400

    created unique constraing on enduser table

--- src/main/java/com/everydaychef/main/model/EndUser.java
+++ src/main/java/com/everydaychef/main/model/EndUser.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -37,10 +38,12 @@ public class EndUser {
   @Length(min = 3, message = "Your username must have at least 3 characters")
   @Length(max = 15, message = "Your username cannot have more than 15 characters")
   @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
+  @Column(unique = true)
   private String userName;
 
   @NotNull(message = "Please provide an email")
   @Email(message = "Please provide a valid email")
+  @Column(unique = true)
   private String email;
 
   @NotNull(message = "Please provide a password")
